2c2823b75083b17489b2086228af67db35410f15
Kourieh Edouard <malkekourie@hotmail.com>
2019-12-26 17:48:37 +0100

    V1 : done

---
+++ ANC3-MiniProject-V1-A-Donner/ANC3-MiniProject-V1-A-Donner/src/miniproject/InvalidTransferException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniproject;
+
+/**
+ *
+ * @author kuo
+ */
+public class InvalidTransferException extends Exception {
+    public InvalidTransferException () {
+        System.out.println("Error !"); ;
+    }
+
+}
